94010abab047edf6c3162bc800c8a44bff071f27
Dmitry Marov <d.marov94@gmail.com>
2019-09-18 23:35:01 +0300

    hello spring

--- src/main/java/dvmarov/app/App.java
+++ src/main/java/dvmarov/app/App.java
@@ -1,13 +1,24 @@
 package dvmarov.app;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
 /**
- * Hello world!
+ * storages server
  *
  */
+@RestController
+@EnableAutoConfiguration
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
     }
 }
