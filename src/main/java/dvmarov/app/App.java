c029534b05a6924b902b3cce43fa59676a05297c
Dmitry Marov <d.marov94@gmail.com>
2019-09-20 23:02:14 +0300

    update

--- src/main/java/dvmarov/app/App.java
+++ src/main/java/dvmarov/app/App.java
@@ -13,8 +13,53 @@ import org.springframework.web.bind.annotation.*;
 public class App 
 {
 
-    @RequestMapping("/")
-    String home() {
+    @GetMapping("/products")
+    String getProducts() {
+        return "Hello World!";
+    }
+
+    @PostMapping("/products")
+    String appendProduct() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/products/{id}")
+    String getProduct() {
+        return "Hello World!";
+    }
+
+    @PatchMapping("/products/{id}")
+    String patchProduct() {
+        return "Hello World!";
+    }
+
+    @DeleteMapping("/products/{id}")
+    String deleteProduct() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/groups")
+    String getGroups() {
+        return "Hello World!";
+    }
+
+    @PostMapping("/groups")
+    String appendGroup() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/groups/{id}")
+    String getGroup() {
+        return "Hello World!";
+    }
+
+    @PatchMapping("/groups/{id}")
+    String patchGroup() {
+        return "Hello World!";
+    }
+
+    @DeleteMapping("/groups/{id}")
+    String deleteGroup() {
         return "Hello World!";
     }
 
