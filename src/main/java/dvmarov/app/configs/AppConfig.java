3882893e639f4d103fe9b5d71d06761028e30d44
Dmitry Marov <d.marov94@gmail.com>
2019-09-22 11:45:17 +0300

    flyway troubles

---
+++ src/main/java/dvmarov/app/configs/AppConfig.java
@@ -0,0 +1,22 @@
+package dvmarov.app.modules.categories;
+import org.springframework.context.annotation.*;
+import org.springframework.hateoas.*;
+
+public class AppConfig {
+
+    // @Bean
+    // public ResourceProcessor<Resource<dvmarov.app.models.Category>> categoriesProcessor() {
+
+    //     return new ResourceProcessor<Resource<dvmarov.app.models.Category>>() {
+
+    //         @Override
+    //         public Resource<dvmarov.app.models.Category> process(Resource<dvmarov.app.models.Category> resource) {
+
+    //         resource.add(new Link("http://localhost:8080/people", "added-link"));
+    //         return resource;
+    //         }
+    //     };
+    // }
+}
+
+
