9f51ba5fe61f1ec8a20a673a737b70d6f4f6def0
Dmitry Marov <d.marov94@gmail.com>
2019-09-22 19:29:58 +0300

    bcrypt auth added

--- src/main/java/dvmarov/app/configs/SecurityConfig.java
+++ src/main/java/dvmarov/app/configs/SecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,22 +24,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.jdbcAuthentication().dataSource(dataSource)
-            .usersByUsernameQuery("select username,password, enabled from users where username=?")
+            .passwordEncoder(passwordEncoder())
+            .usersByUsernameQuery("select username, password, enabled from users where username=?")
             .authoritiesByUsernameQuery("select username, role from user_roles where username=?");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-          .authorizeRequests()
+
+        http.authorizeRequests()
           .anyRequest()
           .authenticated()
           .and()
           .httpBasic();
     }
 
-    // @Bean
-    // public PasswordEncoder passwordEncoder() {
-    //     return new BCryptPasswordEncoder();
-    // }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder(10);
+    }
 }
