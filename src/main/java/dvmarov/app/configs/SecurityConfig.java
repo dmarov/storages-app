e44f4f7564df41d8812b4610ac9da1b82f4e91f3
Dmitry Marov <d.marov94@gmail.com>
2019-09-23 19:44:44 +0300

    security works

--- src/main/java/dvmarov/app/configs/SecurityConfig.java
+++ src/main/java/dvmarov/app/configs/SecurityConfig.java
@@ -1,124 +1,40 @@
 package dvmarov.app.configs;
 
-import javax.sql.DataSource;
+import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-//     @Override
-//     protected void configure(HttpSecurity http) throws Exception {
-
-//         http.authorizeRequests()
-//                 .antMatchers("/api/**").permitAll()
-//                 .anyRequest().authenticated()
-//                 .and()
-//         .formLogin()
-//                 .loginPage("/login")
-//                 .permitAll()
-//                 .and()
-//             .logout()
-//                 .permitAll();
-//     }
-
-//     @Bean
-//     @Override
-//     public UserDetailsService userDetailsService() {
-//         UserDetails user =
-//              User.withDefaultPasswordEncoder()
-//                 .username("user")
-//                 .password("password")
-//                 .roles("USER")
-//                 .build();
-
-//         return new InMemoryUserDetailsManager(user);
-//     }
-//     // @Autowired
-//     // private DataSource dataSource;
-
-//     // @Autowired
-//     // public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
-//     //     // auth.jdbcAuthentication()
-//     //     //     .dataSource(dataSource)
-//     //     //     // .usersByUsernameQuery("select username, password, enabled from user where username=?")
-//     //     //     // .authoritiesByUsernameQuery("select username, role from user_role where username=?")
-//     //   // // .withUser(User.withUsername("user")
-//     //     // // .password(passwordEncoder().encode("pass"))
-//     //     // // .roles("USER"));
-//     //     //     .passwordEncoder(passwordEncoder())
-//     //     //     .usersByUsernameQuery("select username, password, enabled from user where username=?")
-//     //     //     .authoritiesByUsernameQuery("select username, role from user_role where username=?");
-//     // }
-
-//     // @Override
-//     // protected void configure(HttpSecurity http) throws Exception {
-
-//     //     // http.authorizeRequests()
-//     //     //     .anyRequest()
-//     //     //     .authenticated()
-//     //     //     .and()
-//     //     //     .httpBasic();
-
-//     //     // http.sessionManagement()
-//     //     //     .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//     // }
-
-//     // @Bean
-//     // public PasswordEncoder passwordEncoder() {
-
-//     //     return new BCryptPasswordEncoder(10);
-//     // }
-// }
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-      auth.inMemoryAuthentication()
-        .withUser("user").password("{noop}1").roles("USER")
-        .and()
-        .withUser("admin").password("{noop}2ssss").roles("ADMIN");
+        auth.inMemoryAuthentication()
+            .withUser("user").password("{noop}1").roles("USER")
+            .and()
+            .withUser("admin").password("{noop}2").roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.httpBasic()
-            .and()
-            .authorizeRequests()
-            .antMatchers("/api/**").hasRole("ADMIN");
-            // .csrf().disable();
+    http.httpBasic().and()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole("USER", "ADMIN")
+        .antMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
+        .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
+        .antMatchers(HttpMethod.PATCH, "/api/**").hasRole("ADMIN")
+        .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN").and()
+        .csrf().disable()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
