58919bf96d4ec08de8bb1872729f45fb567201e2
Dmitry Marov <d.marov94@gmail.com>
2019-09-23 19:15:53 +0300

    security not working

--- src/main/java/dvmarov/app/configs/SecurityConfig.java
+++ src/main/java/dvmarov/app/configs/SecurityConfig.java
@@ -14,38 +14,111 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//     @Override
+//     protected void configure(HttpSecurity http) throws Exception {
+
+//         http.authorizeRequests()
+//                 .antMatchers("/api/**").permitAll()
+//                 .anyRequest().authenticated()
+//                 .and()
+//         .formLogin()
+//                 .loginPage("/login")
+//                 .permitAll()
+//                 .and()
+//             .logout()
+//                 .permitAll();
+//     }
+
+//     @Bean
+//     @Override
+//     public UserDetailsService userDetailsService() {
+//         UserDetails user =
+//              User.withDefaultPasswordEncoder()
+//                 .username("user")
+//                 .password("password")
+//                 .roles("USER")
+//                 .build();
+
+//         return new InMemoryUserDetailsManager(user);
+//     }
+//     // @Autowired
+//     // private DataSource dataSource;
+
+//     // @Autowired
+//     // public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+//     //     // auth.jdbcAuthentication()
+//     //     //     .dataSource(dataSource)
+//     //     //     // .usersByUsernameQuery("select username, password, enabled from user where username=?")
+//     //     //     // .authoritiesByUsernameQuery("select username, role from user_role where username=?")
+//     //   // // .withUser(User.withUsername("user")
+//     //     // // .password(passwordEncoder().encode("pass"))
+//     //     // // .roles("USER"));
+//     //     //     .passwordEncoder(passwordEncoder())
+//     //     //     .usersByUsernameQuery("select username, password, enabled from user where username=?")
+//     //     //     .authoritiesByUsernameQuery("select username, role from user_role where username=?");
+//     // }
+
+//     // @Override
+//     // protected void configure(HttpSecurity http) throws Exception {
+
+//     //     // http.authorizeRequests()
+//     //     //     .anyRequest()
+//     //     //     .authenticated()
+//     //     //     .and()
+//     //     //     .httpBasic();
+
+//     //     // http.sessionManagement()
+//     //     //     .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//     // }
+
+//     // @Bean
+//     // public PasswordEncoder passwordEncoder() {
+
+//     //     return new BCryptPasswordEncoder(10);
+//     // }
+// }
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private DataSource dataSource;
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-    @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.jdbcAuthentication().dataSource(dataSource)
-            .passwordEncoder(passwordEncoder())
-            .usersByUsernameQuery("select username, password, enabled from users where username=?")
-            .authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+      auth.inMemoryAuthentication()
+        .withUser("user").password("{noop}1").roles("USER")
+        .and()
+        .withUser("admin").password("{noop}2ssss").roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
-            .anyRequest()
-            .authenticated()
+        http.httpBasic()
             .and()
-            .httpBasic();
-
-        http.sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-
-        return new BCryptPasswordEncoder(10);
+            .authorizeRequests()
+            .antMatchers("/api/**").hasRole("ADMIN");
+            // .csrf().disable();
     }
 }
