019628fdbb003b8bfa7539c6a35cd130f67363f0
Dmitry Marov <d.marov94@gmail.com>
2019-09-23 23:18:25 +0300

    vue begin

---
+++ vue-app/webpack.prod.ts
@@ -0,0 +1,9 @@
+import webpack from 'webpack';
+import merge from 'webpack-merge';
+import common from './webpack.common';
+
+const config: webpack.Configuration = merge(common, {
+    mode: 'production',
+});
+
+export default config;
