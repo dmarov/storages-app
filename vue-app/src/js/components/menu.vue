1047b207311f7d9fe71ae795fa84d61b06134014
Dmitry Marov <d.marov94@gmail.com>
2019-09-24 20:35:27 +0300

    update

--- vue-app/src/js/components/menu.vue
+++ vue-app/src/js/components/menu.vue
@@ -7,9 +7,11 @@ export default {
 </script>
 <template lang='pug'>
 
-    nav
-        ul
-            li Продукты
-            li Категории
+    nav.menu
+        ul.menu__items
+            li.menu__item.container__el_pointable
+                a.menu__link.container__el_nolink(href="#") Products
+            li.menu__item.container__el_pointable
+                a.menu__link.container__el_nolink(href="#") Categories
 
 </template>
