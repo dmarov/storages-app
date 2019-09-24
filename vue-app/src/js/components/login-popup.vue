7b2be67d16912631f367eaa8ead77fbab46de813
Dmitry Marov <d.marov94@gmail.com>
2019-09-24 21:40:43 +0300

    update

--- vue-app/src/js/components/login-popup.vue
+++ vue-app/src/js/components/login-popup.vue
@@ -2,13 +2,26 @@
 
 export default {
 
+    data: _ => {
+        return {
+            username: undefined,
+            password: undefined,
+        };
+    },
+    methods: {
+        setUser() {
+        
+        
+        }
+    },
 }
 
 </script>
 <template lang='pug'>
 
     .login-popup
-        input.login-popup__username
-        input.login-popup__password
+        input.login-popup__field(placeholder="username" v-model="username")
+        input.login-popup__field(placeholder="password" type="password" v-model="password")
+        button.login-popup__field(@click='setUser()') Login
 
 </template>
