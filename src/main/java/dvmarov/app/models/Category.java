919993e6aeeb8c75f6a37e52c95e89f44cad8d89
Dmitry Marov <d.marov94@gmail.com>
2019-09-26 19:31:04 +0300

    category append added

--- src/main/java/dvmarov/app/models/Category.java
+++ src/main/java/dvmarov/app/models/Category.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String title;
