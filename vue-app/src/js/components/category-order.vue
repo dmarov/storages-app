0fbce14e2032105c1f3df60666004ce5e1a1d99b
Dmitry Marov <d.marov94@gmail.com>
2019-09-26 20:21:54 +0300

    update

---
+++ vue-app/src/js/components/category-order.vue
@@ -0,0 +1,31 @@
+<script>
+
+import { mapActions } from 'vuex';
+
+export default {
+
+    data: _ => {
+
+        return {
+            title: '',
+            description: '',
+        };
+    },
+    methods: {
+        ...mapActions('categories', [
+            'appendCategory',
+        ]),
+        append() {
+
+            this.appendCategory({
+                title: this.title,
+                description: this.description,
+            });
+        },
+    },
+};
+
+</script>
+<template lang="pug">
+    .category-sort
+</template>
