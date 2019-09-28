b75877e78cb4da2148cef6321d64381310bae70f
Dmitry Marov <d.marov94@gmail.com>
2019-09-28 20:47:38 +0300

    update

--- src/main/java/dvmarov/app/App.java
+++ src/main/java/dvmarov/app/App.java
@@ -4,16 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(App.class);
-    }
+public class App {
 
     public static void main( String[] args ) {
 
