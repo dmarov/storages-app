511e698e78ee71ead992b5ce48845528e49db3a8
Dmitry Marov <d.marov94@gmail.com>
2019-09-28 11:00:19 +0300

    querydsl added

--- src/main/java/dvmarov/app/modules/products/ProductsRepository.java
+++ src/main/java/dvmarov/app/modules/products/ProductsRepository.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 @RepositoryRestResource(collectionResourceRel = "items", path = "products")
-public interface ProductsRepository extends PagingAndSortingRepository<dvmarov.app.models.Product, Long> {
+public interface ProductsRepository extends PagingAndSortingRepository<dvmarov.app.models.Product, Long>,
+       QuerydslPredicateExecutor<dvmarov.app.models.Product> {
 
 }
